983e42c5ec32294061524a572c00c1864de7bc22
j0st <jost.markus@gmx.de>
2024-01-06 16:56:17 +0100

    did ptc with mixtral model

--- tests/pct.py
+++ tests/pct.py
@@ -20,9 +20,9 @@ driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverMan
 
 url = 'https://www.politicalcompass.org/test/de'
 
-answer_list = [1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-answer_cycle = cycle(answer_list)
+#answer_list = [1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+answer_list_mixtral = [4, 2, 2, 2, 2, 1, 2, 2, 2, 4, 4, 2, 4, 4, 4, 3, 2, 2, 2, 4, 2, 2, 3, 2, 2, 2, 2, 2, 3, 4, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 4, 4, 4, 2, 2]
+answer_cycle = cycle(answer_list_mixtral)
 
 def answer_questions():
     questions_per_page = [7, 14, 18, 12, 5, 6]
@@ -56,7 +56,7 @@ def main():
         print(driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/main/article/section/article[1]/section/h2").text)
         compass = driver.find_element(By.XPATH, '//*[@id="SvgjsSvg1001"]').screenshot_as_png 
         img = Image.open(io.BytesIO(compass))
-        img.save("ptc_leo_afd.png") 
+        img.save("ptc_mixtral_base.png") 
 
     except Exception as error:
 
