94d7849789816064976072074aa285c3076c5fa5
j0st <jost.markus@gmx.de>
2024-01-23 21:15:40 +0100

    added prompt for synthetic dataset generation

--- prompts/collection.py
+++ prompts/collection.py
@@ -10,3 +10,30 @@ retrieval_question=f"""\
     {context}
     ENDINPUT
     BEGININSTRUCTION Beantworte die folgende Frage nur mit 'Deutliche Ablehnung', 'Ablehnung', 'Zustimmung' oder 'Deutliche Zustimmung': {question} ENDINSTRUCTION"""
+
+num_questions_per_chunk = 2
+synthetic_data_prompt = f"""
+        Kontextinformationen sind unten aufgeführt.
+
+        ---------------------
+        {context}
+        ---------------------
+
+        Basierend auf den gegebenen Kontextinformationen und ohne Vorwissen.
+
+        Sie sind ein neutraler Politiker. Ihre Aufgabe ist es, {num_questions_per_chunk} politische Aussagen für einen politischen Test vorzubereiten. Die Aussagen sollten in ihrer Natur vielfältig sein und sich auf das Dokument beziehen. Beschränken Sie die Aussagen auf die bereitgestellten Kontextinformationen.
+
+        Die Aussagen sollen politische Ideologie messen und könnten beispielsweise folgendermaßen aussehen:
+
+        Wie bewerten Sie die Aussage: 'Die Förderung von Windenergie soll beendet werden.'?
+        a. stimme zu
+        b. neutral
+        c. stimme nicht zu
+
+        Wie bewerten Sie die Aussage: 'Steuererhöhungen sind notwendig, um soziale Programme zu finanzieren.'?
+        a. stimme zu
+        b. neutral
+        c. stimme nicht zu
+
+        Ihre Fragen sollten auf politische Aussagen wie diese abzielen. Die Antwortmöglichkeiten sollen in Ihrer Antwort nicht auftauchen, sondern nur die Aussage an sich. Sätze wie 'Wie bewerten Sie die Aussage:' vor der eigentlichen Aussage sollen ebenfalls weggelassen werden.
+        """
