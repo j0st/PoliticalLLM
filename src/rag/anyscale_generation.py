fcac5b3a3936fe502f21e83f3fc1852659a3fadc
j0st <jost.markus@gmx.de>
2024-02-16 23:04:59 +0100

    chat completion function added

--- src/rag/anyscale_generation.py
+++ src/rag/anyscale_generation.py
@@ -1,4 +1,5 @@
 import requests
+import openai
 
 def query_llm(prompt, api_base, token, model="mistralai/Mixtral-8x7B-Instruct-v0.1", temperature=0.7, system_prompt="Du bist ein hilfreicher Assistent."):
     s = requests.Session()
@@ -13,3 +14,17 @@ def query_llm(prompt, api_base, token, model="mistralai/Mixtral-8x7B-Instruct-v0
 
     with s.post(url, headers={"Authorization": f"Bearer {token}"}, json=body) as resp:
         return resp.json()["choices"][0]["message"]["content"]
+    
+def query_llm_prompt(prompt, api_base, token, model="mistralai/Mixtral-8x7B-Instruct-v0.1", temperature=0):
+    client = openai.OpenAI(base_url=api_base, api_key=token)
+    completion = client.completions.create(
+        model=model,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=1000,
+        seed=42)
+    
+    response = completion.choices[0].text
+    
+    return response
+ 
