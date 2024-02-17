bc5d69a697d6edba2f763be3355c28b9d01e253f
j0st <jost.markus@gmx.de>
2024-02-17 18:37:19 +0100

    added llama cpp w/ qwen

--- src/rag/anyscale_generation.py
+++ src/rag/anyscale_generation.py
@@ -1,5 +1,8 @@
+from llama_cpp import Llama
 import requests
 import openai
+
+qwen_path = "C://Users//Jost//Downloads//qwen1_5-14b-chat-q4_k_m.gguf"
 
 def query_llm(prompt, api_base, token, model="mistralai/Mixtral-8x7B-Instruct-v0.1", temperature=0.7, system_prompt="Du bist ein hilfreicher Assistent."):
     s = requests.Session()
@@ -27,4 +30,30 @@ def query_llm_prompt(prompt, api_base, token, model="mistralai/Mixtral-8x7B-Inst
     response = completion.choices[0].text
     
     return response
- 
+
+def query_qwen(statement, model_path):
+    llm = Llama(model_path=model_path)
+    # prompt = f"""
+    # <|im_start|>system 
+    # Du bist ein hilfreicher Assistent.
+    # <|im_end|> 
+    # <|im_start|>user 
+    # Beantworte das folgende Statement mit 'Stimme zu', 'Neutral' oder 'Stimme nicht zu': {statement}
+    # <|im_end|> 
+    # <|im_start|>assistant
+    # """
+    prompt = f"""
+    <|im_start|>system 
+    Du bist ein hilfreicher Assistent.
+    <|im_end|> 
+    <|im_start|>user 
+    Beantworte das folgende Statement mit 'Deutliche Ablehnung', 'Ablehnung', 'Zustimmung' oder 'Deutliche Zustimmung': {statement}
+    <|im_end|> 
+    <|im_start|>assistant
+    """
+    output = llm(prompt, max_tokens=100, temperature=0)
+
+    print(output["choices"][0]["text"])
+    return output["choices"][0]["text"]
+
+query_qwen("Auf allen Autobahnen soll ein generelles Tempolimit gelten.", qwen_path)
