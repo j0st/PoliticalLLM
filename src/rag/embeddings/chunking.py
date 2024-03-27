71d3671386632ca67c5d3ce980b1f693b91d6339
j0st <jost.markus@gmx.de>
2024-03-28 00:12:44 +0100

    added sentence chunker

--- src/rag/embeddings/chunking.py
+++ src/rag/embeddings/chunking.py
@@ -1,13 +1,16 @@
-import os
-import re
 import json
+import os
 
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 manifestos_json = os.getenv("FILEPATH_OUTPUT_JSON")
 
-def slide_chunker(manifestos, window_size=2):   
+def slide_chunker(manifestos, window_size=2): # not used
+    """
+    Sets a sliding window to each quasi-sentence and chunks them together.
+    """
+
     with open(manifestos, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
@@ -24,29 +27,11 @@ def slide_chunker(manifestos, window_size=2):
     return texts
 
 def sentence_chunker(manifestos):
-    with open(manifestos, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+    """
+    Aims to combine political statements to full sentences while keeping metadata information.
+    Only quasi-sentences annotated with a policy topic are considered.
+    """
 
-    texts = []
-    sentence = ""
-    for item in data['items']:
-        sub_items = item.get('items', [])
-        for sub_item in sub_items:
-            if not sentence:
-                if sub_item['text'].endswith(('.', '!', '?')):
-                    texts.append(sub_item['text'])
-                else:
-                    sentence += sub_item['text'] + " "
-            else:
-                if sub_item['text'].endswith(('.', '!', '?')):
-                    sentence += sub_item['text']
-                    texts.append(sentence)
-                    sentence = ""
-                else:
-                    sentence += sub_item['text'] + " "
-    return texts
-
-def sentence_chunker2(manifestos):
     with open(manifestos, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
@@ -59,7 +44,7 @@ def sentence_chunker2(manifestos):
         for sub_item in sub_items:
             cmp_code = sub_item.get('cmp_code')
             if cmp_code in ['NA', 'H']:
-                continue  # Ignore items with cmp_code "NA" or "H"
+                continue  # Ignore items with cmp_code "NA" or "H" (headlines etc.)
 
             text = sub_item['text']
             metadata = {
@@ -72,6 +57,7 @@ def sentence_chunker2(manifestos):
 
             if cmp_code == current_cmp_code:
                 current_sentences.append(text)
+
             else:
                 if current_sentences:
                     texts_with_metadata.append((" ".join(current_sentences), metadata))
@@ -83,10 +69,12 @@ def sentence_chunker2(manifestos):
 
     return texts_with_metadata
 
-
-result = sentence_chunker2(manifestos_json)
-
 def concatenate_texts(text_list):
+    """
+    Used for further processing after sentence_chunker().
+    To-do: Combine both functions into one, more simpler function.
+    """
+    
     concatenated_list = []
     current_text = ''
     metadata = None
@@ -109,10 +97,3 @@ def concatenate_texts(text_list):
         concatenated_list.append((current_text, metadata))
 
     return concatenated_list
-
-# Concatenate texts
-concatenated_list = concatenate_texts(result)
-print(concatenated_list[940][0])
-
-#texts = slide_chunker(manifestos_json)
-#texts2 = sentence_chunker(manifestos_json)
