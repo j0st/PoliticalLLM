daedd012cabd2c3b0034404c9ccbe8433a150743
j0st <jost.markus@gmx.de>
2024-03-28 00:13:08 +0100

    improved structure

--- src/rag/retriever.py
+++ src/rag/retriever.py
@@ -1,29 +1,31 @@
+import random
+import os
+
 from chromadb.utils import embedding_functions
 import chromadb
 
-import random
+def retrieve(query, ideology, n_results=3, mode="similarity") -> list:
+    """
+    Retrieves n most similar results from vector database (or randomly if set).
+    Takes a query text and ideology metadata as input.
+    Returns list of results.
+    """
 
-def retrieve(query, ideology, n_results=3, mode="similarity"):
     multilingual_embeddings = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="intfloat/multilingual-e5-base")
-    client = chromadb.PersistentClient(path="C://Users//Jost//Desktop//db_ideologies")
+    db_path = os.getenv("VECTOR_DATABASE")
+    client = chromadb.PersistentClient(path=db_path)
     manifesto_collection = client.get_or_create_collection(name="manifesto-db", embedding_function=multilingual_embeddings)
-    contexts = []
+
     if mode == "similarity":
         retrieved_context = manifesto_collection.query(query_texts=[query], n_results=n_results, where={"ideology": ideology})
-        context = retrieved_context['documents'][0][0]
-        context_1 = retrieved_context['documents'][0][1]
-        context_2 = retrieved_context['documents'][0][2]
-        contexts.append(context)
-        contexts.append(context_1)
-        contexts.append(context_2)
+        contexts = [context for context in retrieved_context['documents']]
+        contexts = contexts[0]
     
-    else:
+    elif mode == "random":
         retrieved_context = manifesto_collection.get(ids=["id_ar_240", "id_ar_1970", "id_ar_1980"], where={"ideology": ideology})
-        context = retrieved_context['documents'][0]
-        context_1 = retrieved_context['documents'][1]
-        context_2 = retrieved_context['documents'][2]
-        contexts.append(context)
-        contexts.append(context_1)
-        contexts.append(context_2)
+        contexts = [context for context in retrieved_context['documents']]
+
+    else:
+        raise ValueError(f"Unsupported mode: {mode}")
 
     return contexts
