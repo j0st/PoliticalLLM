7793058f37a139db5357f05234c835c758cd1ec1
j0st <jost.markus@gmx.de>
2024-03-28 01:21:37 +0100

    refactor

--- src/rag/embeddings/embeddings.py
+++ src/rag/embeddings/embeddings.py
@@ -1,66 +1,50 @@
-import src.rag.embeddings.chunking as chunking_function
 from chromadb.utils import embedding_functions
 import chromadb
 
-data = "data\manifestos.json"
-data_ideology = ["data\manifestos_al.json", "data\manifestos_ar.json", "data\manifestos_ll.json", "data\manifestos_lr.json"]
+import chunking as chunking_function
 
-#docs = chunking_function.slide_chunker(data)
-docs_al, docs_ar, docs_ll, docs_lr = [chunking_function.sentence_chunker(data) for data in data_ideology]
+ideology_manifestos = ["data\Authoritarian-left-manifestos.json",
+                       "data\Authoritarian-right-manifestos.json",
+                       "data\Libertarian-left-manifestos.json",
+                       "data\Libertarian-right-manifestos.json"]
 
-ids_al = [f"id_al_{i}" for i in range(len(docs_al))]
-ids_ar = [f"id_ar_{i}" for i in range(len(docs_ar))]
-ids_ll = [f"id_ll_{i}" for i in range(len(docs_ll))]
-ids_lr = [f"id_lr_{i}" for i in range(len(docs_lr))]
+# chunking
+docs_al, docs_ar, docs_ll, docs_lr = [chunking_function.sentence_chunker(manifesto) for manifesto in ideology_manifestos]
+chunked_docs_al, chunked_docs_ar, chunked_docs_ll, chunked_docs_lr = [chunking_function.concatenate_texts(doc) for doc in [docs_al, docs_ar, docs_ll, docs_lr]]
 
-metadatas_al = [{"ideology": "Authoritarian-left"} for _ in range(len(ids_al))]
-metadatas_ar = [{"ideology": "Authoritarian-right"} for _ in range(len(ids_ar))]
-metadatas_ll = [{"ideology": "Libertarian-left"} for _ in range(len(ids_ll))]
-metadatas_lr = [{"ideology": "Libertarian-right"} for _ in range(len(ids_lr))]
+# metadata
+ids_al = [f"id_al_{i}" for i in range(len(chunked_docs_al))]
+ids_ar = [f"id_ar_{i}" for i in range(len(chunked_docs_ar))]
+ids_ll = [f"id_ll_{i}" for i in range(len(chunked_docs_ll))]
+ids_lr = [f"id_lr_{i}" for i in range(len(chunked_docs_lr))]
 
+metadata_al = [item[1] for item in chunked_docs_al]
+metadata_ar = [item[1] for item in chunked_docs_ar]
+metadata_ll = [item[1] for item in chunked_docs_ll]
+metadata_lr = [item[1] for item in chunked_docs_lr]
+
+# get list of statement for embeddings
+list_of_statements_al = [item[0] for item in chunked_docs_al]
+list_of_statements_ar = [item[0] for item in chunked_docs_ar]
+list_of_statements_ll = [item[0] for item in chunked_docs_ll]
+list_of_statements_lr = [item[0] for item in chunked_docs_lr]
+
+# create embeddings
 multilingual_embeddings = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="jost/multilingual-e5-base-politics-de")
 
-client = chromadb.PersistentClient(path="C://Users//Jost//Desktop//db_ideologies") # save vector database locally
-manifesto_collection = client.get_or_create_collection(name="manifesto-db-ft", embedding_function=multilingual_embeddings)
+client = chromadb.PersistentClient(path="C://Users//Jost//Desktop//db_TEST_AR") # save vector database locally
+manifesto_collection = client.get_or_create_collection(name="manifesto-db-ar", embedding_function=multilingual_embeddings)
 
-# manifesto_collection.add(documents=docs_al, ids=ids_al, metadatas=metadatas_al)
-# manifesto_collection.add(documents=docs_ar, ids=ids_ar, metadatas=metadatas_ar)
-# manifesto_collection.add(documents=docs_ll, ids=ids_ll, metadatas=metadatas_ll)
-# manifesto_collection.add(documents=docs_lr, ids=ids_lr, metadatas=metadatas_lr)
+manifesto_collection.add(documents=list_of_statements_al, ids=ids_al, metadatas=metadata_al)
+manifesto_collection.add(documents=list_of_statements_ar, ids=ids_ar, metadatas=metadata_ar)
+manifesto_collection.add(documents=list_of_statements_ll, ids=ids_ll, metadatas=metadata_ll)
+manifesto_collection.add(documents=list_of_statements_lr, ids=ids_lr, metadatas=metadata_lr)
 
-# results = manifesto_collection.query(
-#     query_texts=["Auf allen Autobahnen soll ein generelles Tempolimit gelten."],
-#     n_results=3,
-#     where={"ideology": "Authoritarian-left"}
-# )
+results = manifesto_collection.query(
+    query_texts=["Auf allen Autobahnen soll ein generelles Tempolimit gelten."],
+    n_results=3,
+    where={"ideology": "Authoritarian-right"}
+)
 
-
-# print(results)
-
-# results = manifesto_collection.query(
-#     query_texts=["Auf allen Autobahnen soll ein generelles Tempolimit gelten."],
-#     n_results=3,
-#     where={"ideology": "Authoritarian-right"}
-# )
-
-# print(results)
-
-# results = manifesto_collection.query(
-#     query_texts=["Auf allen Autobahnen soll ein generelles Tempolimit gelten."],
-#     n_results=3,
-#     where={"ideology": "Libertarian-left"}
-# )
-
-
-# print(results)
-
-# results = manifesto_collection.query(
-#     query_texts=["Auf allen Autobahnen soll ein generelles Tempolimit gelten."],
-#     n_results=3,
-#     where={"ideology": "Libertarian-right"}
-# )
-
-
-# print(results)
-
+print(results)
 print(manifesto_collection.count())
