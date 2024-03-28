2739743e70c88cfd15154d00c4cae4b2ace1883b
j0st <jost.markus@gmx.de>
2024-03-28 11:59:51 +0100

    comment

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from llms import LLM
 
-mixtral = LLM("Mixtral")
-mixtral.pct("Test2603", iterations=1)
+if __name__ == "__main__":
+    # Define and run test setups here
+    mixtral = LLM("Mixtral-8x7B-Instruct-v0.1")
+    mixtral.pct("TEST2803", iterations=2)
