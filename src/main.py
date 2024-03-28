9feb4e4a5f6437ba7ecffd7289a8f2a8a6613ddb
j0st <jost.markus@gmx.de>
2024-03-28 12:31:07 +0100

    random rag example

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ from llms import LLM
 if __name__ == "__main__":
     # Define and run test setups here
     mixtral = LLM("Mixtral-8x7B-Instruct-v0.1")
-    mixtral.pct("TEST2803", iterations=2)
+    #mixtral.pct("TEST2803", iterations=2)
+    mixtral.pct("TEST2803_2", party="Afd", ideology="Authoritarian-right", n_results=2, rag=True, rag_mode="random")
