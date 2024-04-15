ae362b25ce804f0c1b644dab419d3499f945470a
j0st <jost.markus@gmx.de>
2024-04-15 23:30:20 +0200

    fixed statement chunking

--- src/rag/embeddings/chunking.py
+++ src/rag/embeddings/chunking.py
@@ -1,10 +1,4 @@
 import json
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-manifestos_json = os.getenv("FILEPATH_OUTPUT_JSON")
 
 def slide_chunker(manifestos, window_size=2): # not used
     """
@@ -26,74 +20,72 @@ def slide_chunker(manifestos, window_size=2): # not used
 
     return texts
 
-def sentence_chunker(manifestos):
+def get_full_sentence(manifesto_chunks):
     """
-    Aims to combine political statements to full sentences while keeping metadata information.
+    Concatenates political statements to full sentences.
+    """
+
+    i = 0
+    while i < len(manifesto_chunks) - 1:
+        if not manifesto_chunks[i]['text'][-1] in '.!?':
+            manifesto_chunks[i]['text'] += ' ' + manifesto_chunks[i+1]['text']
+            manifesto_chunks[i]['cmp_code'] += '+' + manifesto_chunks[i+1]['cmp_code']
+            del manifesto_chunks[i+1]
+        else:
+            i += 1
+
+    return manifesto_chunks
+
+def statement_chunker(manifestos):
+    """
+    Aims to combine quasi-sentences to political statements while keeping metadata information.
     Only quasi-sentences annotated with a policy topic are considered.
     """
+
+    concatenated_data = []
+    current_cmp_code = None
+    current_text = ""
 
     with open(manifestos, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    texts_with_metadata = []
-    current_sentences = []
-    current_cmp_code = None
-
-    for item in data['items']:
-        sub_items = item.get('items', [])
-        for sub_item in sub_items:
-            cmp_code = sub_item.get('cmp_code')
+    for manifesto in data['items']:
+        texts_and_metadatas = manifesto.get('items', [])
+        for quasi_sentence in texts_and_metadatas:
+            cmp_code = quasi_sentence.get('cmp_code')
             if cmp_code in ['NA', 'H']:
                 continue  # Ignore items with cmp_code "NA" or "H" (headlines etc.)
 
-            text = sub_item['text']
-            metadata = {
-                'cmp_code': cmp_code,
-                'partyname': sub_item.get('name'),
-                'abbreviation': sub_item.get('abbreviation'),
-                'date': sub_item.get('date'),
-                'ideology': sub_item.get('political_orientation')
-            }
+            if quasi_sentence['cmp_code'] != current_cmp_code: # If cmp_code has changed, add the previous concatenated text to the list
+                if current_text:
+                    concatenated_data.append({
+                        'text': current_text.strip(),
+                        'cmp_code': current_cmp_code,
+                        'eu_code': quasi_sentence['eu_code'],
+                        'name': quasi_sentence['name'],
+                        'abbreviation': quasi_sentence['abbreviation'],
+                        'date': quasi_sentence['date'],
+                        'political_orientation': quasi_sentence['political_orientation']
+                    })
 
-            if cmp_code == current_cmp_code:
-                current_sentences.append(text)
+                current_text = quasi_sentence['text']
+                current_cmp_code = quasi_sentence['cmp_code']
 
             else:
-                if current_sentences:
-                    texts_with_metadata.append((" ".join(current_sentences), metadata))
-                current_sentences = [text]
-                current_cmp_code = cmp_code
+                current_text += " " + quasi_sentence['text'] # Concatenate text if cmp_code remains the same
 
-    if current_sentences:
-        texts_with_metadata.append((" ".join(current_sentences), metadata))
+        # Append last quasi-sentence
+        if current_text:
+            concatenated_data.append({
+                'text': current_text.strip(),
+                'cmp_code': current_cmp_code,
+                'eu_code': texts_and_metadatas[-1]['eu_code'],
+                'name': texts_and_metadatas[-1]['name'],
+                'abbreviation': texts_and_metadatas[-1]['abbreviation'],
+                'date': texts_and_metadatas[-1]['date'],
+                'political_orientation': texts_and_metadatas[-1]['political_orientation']
+            })
+    
+    results = get_full_sentence(concatenated_data)
 
-    return texts_with_metadata
-
-def concatenate_texts(text_list):
-    """
-    Used for further processing after sentence_chunker().
-    To-do: Combine both functions into one, more simpler function.
-    """
-
-    concatenated_list = []
-    current_text = ''
-    metadata = None
-
-    for text, meta in text_list:
-        if text.endswith(('.', '!', '?')):
-            if current_text:
-                concatenated_list.append((current_text + ' ' + text, metadata))
-                current_text = ''
-            else:
-                concatenated_list.append((text, meta))
-        else:
-            if not current_text:
-                current_text = text
-                metadata = meta
-            else:
-                current_text += ' ' + text
-
-    if current_text:
-        concatenated_list.append((current_text, metadata))
-
-    return concatenated_list
+    return results
